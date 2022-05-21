b98a89c79bb54360677beba84b3897da7d74fb1b
Stephan Seitz <stephan.seitz@fau.de>
2022-05-22 00:22:16 +0200

    "Fix" (?) closest_hit shader with lots of extensions

--- shaders/closest_hit.glsl
+++ shaders/closest_hit.glsl
@@ -3,23 +3,36 @@
 
 #extension GL_EXT_ray_tracing : require
 #extension GL_EXT_nonuniform_qualifier : enable
+#extension GL_EXT_scalar_block_layout : enable
+#extension GL_EXT_buffer_reference : enable
 
 layout(location = 0) rayPayloadInEXT vec3 hitValue;
 
 hitAttributeEXT vec3 attribs;
 layout(binding = 0, set = 0) uniform accelerationStructureEXT topLevelAS;
 
-layout(shaderRecordEXT) buffer Indices { uint i[]; } indices;
-layout(shaderRecordEXT) buffer Normals { vec3 v[]; } normals;
+layout(buffer_reference, buffer_reference_align = 8, scalar)
+buffer NormalBuffer {
+  vec3 n[];
+};
+layout(buffer_reference, buffer_reference_align = 8, scalar)
+buffer IndexBuffer {
+  uvec3 i[];
+};
+
+layout(shaderRecordEXT, std430) buffer SBT {
+  IndexBuffer indices;
+  NormalBuffer normals;
+};
 
 void main()
 {
   const vec3 barycentrics = vec3(1.0 - attribs.x - attribs.y, attribs.x, attribs.y);
 
-  ivec3 index = ivec3(indices.i[3 * gl_PrimitiveID], indices.i[3 * gl_PrimitiveID + 1], indices.i[3 * gl_PrimitiveID + 2]);
-  vec3 n0 = normals.v[3 * index.x + 0];
-  vec3 n1 = normals.v[3 * index.y + 1];
-  vec3 n2 = normals.v[3 * index.z + 2];
+  uvec3 index = indices.i[gl_PrimitiveID];
+  vec3 n0 = normals.n[3 * index.x + 0];
+  vec3 n1 = normals.n[3 * index.y + 1];
+  vec3 n2 = normals.n[3 * index.z + 2];
   vec3 normal = normalize(n0 * barycentrics.x + n1 * barycentrics.y + n2 * barycentrics.z);
   hitValue = vec3(1.0, normal.xy);
   
