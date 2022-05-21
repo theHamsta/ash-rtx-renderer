527bf336d04f73e49d6a72b4998d512a352705e8
Stephan Seitz <stephan.seitz@fau.de>
2022-05-22 00:24:36 +0200

    use GL_EXT_buffer_reference2 instead of GL_EXT_buffer_reference

--- shaders/closest_hit.glsl
+++ shaders/closest_hit.glsl
@@ -4,7 +4,7 @@
 #extension GL_EXT_ray_tracing : require
 #extension GL_EXT_nonuniform_qualifier : enable
 #extension GL_EXT_scalar_block_layout : enable
-#extension GL_EXT_buffer_reference : enable
+#extension GL_EXT_buffer_reference2 : enable
 
 layout(location = 0) rayPayloadInEXT vec3 hitValue;
 
